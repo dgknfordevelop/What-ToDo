bf14e739a10d84cfbbda7e1ed776f1de4ebe2435
Doğukan Çalışkan <dogukan.engt@gmail.com>
2021-11-29 20:37:50 +0300

    building scss and JavaScript

--- toDo.js
+++ toDo.js
@@ -1,6 +1,29 @@
 let userInput = document.querySelector(".note-input");
 let addButton = document.querySelector(".note-add-button");
+let noteSection = document.querySelector(".main-note-section");
 let deleteSingleNote = document.querySelector(".note-delete");
 let deleteAll = document.querySelector(".delete-all-container");
 
+addButton.addEventListener("click", addNote)
+
+function addNote(){
+
+    let newNoteLine = document.createElement("div");
+    let newLineAttribute = document.createAttribute("class");
+    newLineAttribute.value = "note-line";
+    newNoteLine.setAttributeNode(newLineAttribute)
+
+
+    let newListItem = document.createElement("div");
+    newListItem.textContent = userInput.value;
+    let newItemAttribute = document.createAttribute("class");
+    newItemAttribute.value = "note-item";
+    newListItem.setAttributeNode(newItemAttribute)
+
+    newNoteLine.append(newListItem);
+    noteSection.append(newNoteLine);
+
+
+
+}
 
