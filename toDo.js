bcd758e73ac4004ac7f18611671f77b41564fe71
Doğukan Çalışkan <dogukan.engt@gmail.com>
2021-12-01 02:20:04 +0300

    updates on scss and JavaScript

--- toDo.js
+++ toDo.js
@@ -1,29 +1,22 @@
 let userInput = document.querySelector(".note-input");
 let addButton = document.querySelector(".note-add-button");
 let noteSection = document.querySelector(".main-note-section");
-let deleteSingleNote = document.querySelector(".note-delete");
-let deleteAll = document.querySelector(".delete-all-container");
 
-addButton.addEventListener("click", addNote)
+addButton.addEventListener("click", addNote);
 
-function addNote(){
+function addNote() {
 
-    let newNoteLine = document.createElement("div");
-    let newLineAttribute = document.createAttribute("class");
-    newLineAttribute.value = "note-line";
-    newNoteLine.setAttributeNode(newLineAttribute)
+    if (userInput.value.length == 0) {
+        alert("Please write something to add a note")
+        
+    } else {
+        let newNoteLine = document.createElement("div");
+        newNoteLine.className = "note-line";
 
-
-    let newListItem = document.createElement("div");
-    newListItem.textContent = userInput.value;
-    let newItemAttribute = document.createAttribute("class");
-    newItemAttribute.value = "note-item";
-    newListItem.setAttributeNode(newItemAttribute)
-
-    newNoteLine.append(newListItem);
-    noteSection.append(newNoteLine);
-
-
+        noteSection.innerHTML += `<div class="note-line">
+        <div class="note-item">${userInput.value}</div>
+        <input type="checkbox" class="note-control">
+        <div class="note-delete">x</div></div>`
+    }
 
 }
-
