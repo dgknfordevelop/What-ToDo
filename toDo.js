414f2097baf18c120317eb57a57c6c479206c875
Doğukan Çalışkan <dogukan.engt@gmail.com>
2021-12-07 03:36:35 +0300

    updates on Scss and JavaScript

--- toDo.js
+++ toDo.js
@@ -3,11 +3,13 @@ let addButton = document.querySelector(".note-add-button");
 let noteSection = document.querySelector(".main-note-section");
 let deleteAllButton = document.querySelector(".delete-all-button");
 let checkItem = document.querySelector(".note-check");
-let noteItem = document.querySelector("note-item");
+let noteItem = document.querySelector(".note-item");
 
 addButton.addEventListener("click", addNote);
 noteSection.addEventListener("click", deleteSingleNote);
 deleteAllButton.addEventListener("click", deleteAll);
+
+
 
 function addNote() {
 
@@ -18,10 +20,9 @@ function addNote() {
         noteSection.innerHTML += `<div class="note-line">
         <div class="note-item">${userInput.value}</div>
         <div class="note-check">
-        <input type="checkbox" ></div>
+        <input type="checkbox"></div>
         <div class="note-delete"><i class="far fa-trash-alt"></i></div></div>`
         userInput.value = "";
-
     }
 }
 
