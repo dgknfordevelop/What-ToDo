fc2833ba2b614513d3046d0e61617cc1226f8504
Doğukan Çalışkan <dogukan.engt@gmail.com>
2021-12-09 20:43:54 +0300

    updates on Scss and JavaScript

--- js/counter.js
+++ js/counter.js
@@ -1,2 +1,23 @@
 let counterNumber = document.querySelector(".counter-number");
 let plusMinus = document.querySelector(".plus-minus-container");
+let resetButton = document.querySelector(".reset-button");
+
+plusMinus.addEventListener("click", changeNumber);
+resetButton.addEventListener("click", resetNumber);
+
+function changeNumber(plusButton){
+    if(plusButton.target.className == "far fa-plus-square"){
+
+        let IncrementNum = Number(counterNumber.textContent) + 1;
+        counterNumber.textContent = IncrementNum;
+
+    } else if(plusButton.target.className == "far fa-minus-square"){
+
+        let IncrementNum = Number(counterNumber.textContent) - 1;
+        counterNumber.textContent = IncrementNum;
+    }
+}
+
+function resetNumber(){
+    counterNumber.textContent = 0;
+}
--- js/digital-clock.js
+++ js/digital-clock.js
@@ -1,31 +1,33 @@
+let clockContainer = document.querySelector(".digital-clock-container");
 let hoursBox = document.querySelector(".hours");
 let minutesBox = document.querySelector(".minutes");
 let secondsBox = document.querySelector(".seconds");
 
-function timeSet(){
-    
-let digitalDate = new Date();
-let digitalHours = digitalDate.getHours();
-let digitalMinutes = digitalDate.getMinutes();
-let digitalSeconds = digitalDate.getSeconds();
+function timeSet() {
 
-    if (digitalHours > 0 && digitalHours < 10) {
-        hoursBox.textContent = `0${digitalSeconds}`;
+    let digitalDate = new Date();
+    let digitalHours = digitalDate.getHours();
+    let digitalMinutes = digitalDate.getMinutes();
+    let digitalSeconds = digitalDate.getSeconds();
+
+
+    if (digitalHours >= 0 && digitalHours < 10) {
+        hoursBox.textContent = `0${digitalHours}`;
     } else {
         hoursBox.textContent = digitalHours;
     };
 
-    if (digitalMinutes > 0 && digitalMinutes < 10) {
-        minutesBox.textContent = `0${digitalSeconds}`;
+    if (digitalMinutes >= 0 && digitalMinutes < 10) {
+        minutesBox.textContent = `0${digitalMinutes}`;
     } else {
         minutesBox.textContent = digitalMinutes;
     };
 
-    if (digitalSeconds > 0 && digitalSeconds < 10) {
+    if (digitalSeconds >= 0 && digitalSeconds < 10) {
         secondsBox.textContent = `0${digitalSeconds}`;
     } else {
         secondsBox.textContent = digitalSeconds;
-    }
+    };
 
 }
 
